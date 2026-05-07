4e7010c5d0e0f6fcaf6dbdf26cde06574afeae6a
JohnnyTao <ahalamora@163.com>
2026-05-07 10:40:47 +0800

    feat: add integer division on master

--- calculator.py
+++ calculator.py
@@ -10,7 +10,12 @@ def multiply(a, b):
     return a * b
 
 
+def divide(a, b):
+    return a // b
+
+
 if __name__ == "__main__":
     print("3 + 5 =", add(3, 5))
     print("10 - 4 =", subtract(10, 4))
     print("2 * 3 =", multiply(2, 3))
+    print("10 // 4 =", divide(10, 4))
