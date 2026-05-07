19188a03febde5c17c4e8d4cbd4141bca659ad76
JohnnyTao <ahalamora@163.com>
2026-05-07 09:46:50 +0800

    feat: add calculator with add, subtract and multiply

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("3 + 5 =", add(3, 5))
+    print("10 - 4 =", subtract(10, 4))
+    print("2 * 3 =", multiply(2, 3))
