29fd7bb491291f351506678be50d5e5d04695855
JohnnyTao <ahalamora@163.com>
2026-05-07 21:06:45 +0800

    feat: add square function

--- calculator.py
+++ calculator.py
@@ -22,6 +22,10 @@ def modulo(a, b):
     return a % b
 
 
+def square(a):
+    return a * a
+
+
 if __name__ == "__main__":
     print("3 + 5 =", add(3, 5))
     print("10 - 4 =", subtract(10, 4))
@@ -29,3 +33,4 @@ if __name__ == "__main__":
     print("10 // 4 =", divide_integer(10, 4))
     print("10 / 4 =", divide(10, 4))
     print("10 % 3 =", modulo(10, 3))
+    print("3^2 =", square(3))
